7f253e8fb6f68f56ea6cf065861d9af667642891
ITH <iterceiro.formacion@gmail.com>
2026-01-29 18:57:13 +0100

    5: extract Local Variable

--- src/com/refactoring/exercises/Ejercicio05.java
+++ src/com/refactoring/exercises/Ejercicio05.java
@@ -6,6 +6,10 @@ package com.refactoring.exercises;
 public class Ejercicio05 {
 
     public double calcularPrecioFinal(double precioBase, double tasaImpuesto, double descuento) {
-        return precioBase * (1 + tasaImpuesto) * (1 - descuento);
+        double factorTasaImpuesto = 1 + tasaImpuesto;
+		double factorDescuento = 1 - descuento;
+		double precioConImpuestos = precioBase * factorTasaImpuesto;
+		double precioConDescuento = precioConImpuestos * factorDescuento;
+		return precioConDescuento;
     }
 }
