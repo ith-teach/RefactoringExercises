6df38805a589e6654cb449c4b6ce2ce470e81bbb
ith <iterceiro.formacion@gmail.com>
2026-01-30 19:26:46 +0100

    9: Move method

--- src/com/refactoring/exercises/ejercicio09/Cliente.java
+++ src/com/refactoring/exercises/ejercicio09/Cliente.java
@@ -2,9 +2,18 @@ package com.refactoring.exercises.ejercicio09;
 
 public class Cliente {
 
-    private boolean vip;
+    private static final double DESCUENTO_BASE = 0.05;
+	private static final double DESCUENTO_VIP = 0.15;
+	private boolean vip;
     
     public boolean esVIP() {
         return vip;
     }
+
+	public double calcularDescuento(double monto) {
+	    if (esVIP()) {
+	        return monto * DESCUENTO_VIP;
+	    }
+	    return monto * DESCUENTO_BASE;
+	}
 }
