d100c0cf8872a11feffa647b4b71bce8853f2d24
ITH <iterceiro.formacion@gmail.com>
2026-02-05 19:07:29 +0100

    12: Replace Conditional to Polymorfism

--- src/com/refactoring/exercises/Ejercicio12.java
+++ src/com/refactoring/exercises/Ejercicio12.java
@@ -1,16 +1,23 @@
 package com.refactoring.exercises;
 
+import com.refactoring.exercises.ejercicio12.Designer;
+import com.refactoring.exercises.ejercicio12.Developer;
+import com.refactoring.exercises.ejercicio12.Empleado;
+
 //EJERCICIO 12: Replace Conditional with Polymorphism (Reemplazar Condicional con Polimorfismo)
 //Reemplaza el switch con polimorfismo usando herencia
 
 public class Ejercicio12 {
 
     public double calcularSalario(String tipo, double horasTrabajadas) {
+    	Empleado empleado;
         switch (tipo) {
             case "DESARROLLADOR":
-                return horasTrabajadas * 50;
+            	empleado = new Developer(horasTrabajadas);
+                return empleado.calcularSalario();
             case "DISEÑADOR":
-                return horasTrabajadas * 40;
+            	empleado = new Designer(horasTrabajadas);
+                return empleado.calcularSalario();
             case "TESTER":
                 return horasTrabajadas * 35;
             default:
