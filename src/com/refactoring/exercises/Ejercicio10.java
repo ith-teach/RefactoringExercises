49537991be74a3d9e29bc87e63e67f4d61bb0e04
ITH <iterceiro.formacion@gmail.com>
2026-02-05 18:04:51 +0100

    10: Change Method Signature

--- src/com/refactoring/exercises/Ejercicio10.java
+++ src/com/refactoring/exercises/Ejercicio10.java
@@ -5,7 +5,14 @@ package com.refactoring.exercises;
 
 public class Ejercicio10 {
 
-    public String crearMensaje(String nombre, String apellido, int edad) {
+    /**
+	 * @deprecated Use {@link #crearMensaje(int,String,String,String)} instead
+	 */
+	public String crearMensaje(String nombre, String apellido, int edad) {
+		return crearMensaje(edad, nombre, apellido, "Sr./Sra.");
+	}
+
+	public String crearMensaje(int edad, String nombre, String apellido, String titulo) {
         return "Nombre: " + nombre + " " + apellido + ", Edad: " + edad;
     }
 }
