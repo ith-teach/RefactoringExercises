1af570d91816d78abf7d3d69367ed0854c304ff7
ith <iterceiro.formacion@gmail.com>
2026-02-06 17:50:29 +0100

    10: Change Method Signature

--- src/com/refactoring/exercises/Ejercicio10.java
+++ src/com/refactoring/exercises/Ejercicio10.java
@@ -1,11 +1,19 @@
 package com.refactoring.exercises;
 
 //EJERCICIO 10: Change Method Signature (Cambiar Firma del Método)
-//Cambia el orden de los parámetros y agrega un parámetro con valor por defecto
+//Cambia el orden de los parámetros y 
+// un parámetro con valor por defecto
 
 public class Ejercicio10 {
 
-    public String crearMensaje(String nombre, String apellido, int edad) {
+    /**
+	 * @deprecated Use {@link #crearMensaje(int,String,String,String)} instead
+	 */
+	public String crearMensaje(String nombre, String apellido, int edad) {
+		return crearMensaje(edad, nombre, apellido, "Sr./Sra.");
+	}
+
+	public String crearMensaje(int edad, String nombre, String apellido, String titulo) {
         return "Nombre: " + nombre + " " + apellido + ", Edad: " + edad;
     }
 }
